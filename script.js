fde687d4ab46f06f91eb217629dac43c4ac85234
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-05-02 00:43:17 -0500

    validating done

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // Write your JavaScript code here!
-
 window.addEventListener("load", function() {
+    let formSubmit = document.getElementById("formSubmit");
+    let faultyItems = document.getElementById("faultyItems");
+    let pilotName = document.querySelector('input[name=pilotName]');
+    let copilotName = document.querySelector('input[name=copilotName]');
+    let fuelLevel = document.querySelector('input[name=fuelLevel]');
+    let cargoMass = document.querySelector('input[name=cargoMass]');
+    let pilotStatus = document.getElementById('pilotStatus');
+    let copilotStatus = document.getElementById('copilotStatus');
+    let fuelStatus = document.querySelector('[data-testid=fuelStatus]');
+    let cargoStatus = document.querySelector('[data-testid=cargoStatus]');
+    let launchStatus = document.getElementById('launchStatus');
 
+    formSubmit.addEventListener("click", function(event) {
+        event.preventDefault();
+        formSubmission(window, faultyItems, pilotName.value, copilotName.value, fuelLevel.value, cargoMass.value);
+    })
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
     let listedPlanetsResponse;
@@ -12,5 +26,6 @@ window.addEventListener("load", function() {
         console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
     })
-    
+    // add event listener test validation in live server browser. see what happens when submit and use dev tools console
+    // unfamiliar errors alert is not defined. keep working towards page running and use console log to see what happening
  });
--- scriptHelper.js
+++ scriptHelper.js
@@ -18,10 +18,39 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
  }
  
  function validateInput(testInput) {
-    
+    if (testInput.length === 0) {
+        return "Empty";
+    } else if (isNaN(testInput)) {
+        return "Not a Number";
+    } else {
+        return "Is a Number";
+    }
  }
  
  function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    if (pilot === "" || copilot === "" || fuelLevel === "" || cargoLevel === "") {
+        window.alert("All fields are required!")
+    } else if (validateInput(copilot) === "Is a Number" || validateInput(pilot) === "Is a Number") {
+        window.alert("Pilot and Co-pilot Names must be strings!");
+    } else if (validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
+        window.alert("Fuel Level and Cargo Mass must be numbers!");
+    }  else {
+        faultyItems.style.visibility = "visible";
+        pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+        copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+        fuelStatus.innerHTML = `Fuel level ${(fuelLevel < 10000) ? "too low" : "high enough"} for launch`;
+        cargoStatus.innerHTML = `Cargo mass ${(cargoLevel > 10000) ? "too heavy" : "low enough"} for launch`;
+        fuelLevel < 10000 || cargoLevel > 10000 ? launchStatus.style.color = "red" : launchStatus.style.color = "green"
+    }
+    // this is the definition not the fucniton call
+    // takes in parameters
+    // document parameter, input from form, and a paramter that reps html element list final launch checklist
+    // does not allow empty field, should give window alert
+    // polot and oc should be strings other numbers, gfives window alert that gives feedbak about expeted inputs
+    // use validate input to help with this
+    // if it passes this round of validaiton then user inputs are good and can move on
+    // still in form submission but determining output based on mass
+    // shuttle ready for launch changes on input but nothing else
     
  }
  
